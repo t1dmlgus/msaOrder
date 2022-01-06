4e9d1c039d2d2e21debc4624ed9f5a6efc342e6d
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-06 16:22:13 +0900

    init

---
+++ back/src/main/java/com/t1dmlgus/msaOrder/MsaOrderApplication.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.msaOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaOrderApplication.class, args);
+	}
+
+}
